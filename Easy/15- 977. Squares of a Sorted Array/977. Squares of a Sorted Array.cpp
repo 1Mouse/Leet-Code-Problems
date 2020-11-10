b80c07957717714852f898c2b8e94d2aaeab19bd
Mohamed Elfar <melfar391@gmail.com>
2020-11-10 15:03:20 +0200

    solved using two pointers and configuring built in sort

---
+++ Easy/15- 977. Squares of a Sorted Array/977. Squares of a Sorted Array.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &A)
+    {
+        sort(A.begin(), A.end(), cmp);
+        for (int i = 0; i < A.size(); i++)
+        {
+            A[i] = A[i] * A[i];
+        }
+        return A;
+    }
+
+    static bool cmp(int x, int y)
+    {
+        return x * x < y * y;
+    }
+};
