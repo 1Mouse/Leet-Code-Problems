6bd3a9cb6f789d2dbea133ba6984a11200855e01
Mohamed Elfar <melfar391@gmail.com>
2021-03-22 08:37:45 +0200

    a decent one solved by converting into vectors

---
+++ Easy/24- 234. Palindrome Linked List/234. Palindrome Linked List.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//Easy solution by converting into vectors but it's slow and uses memory
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        vector<int> v;
+        while (head)
+        {
+            v.push_back(head->val);
+            head = head->next;
+        }
+
+        int start = 0, end = v.size() - 1;
+
+        while (start < end)
+        {
+            if (v[start++] != v[end--])
+                return false;
+        }
+
+        return true;
+    }
+};
