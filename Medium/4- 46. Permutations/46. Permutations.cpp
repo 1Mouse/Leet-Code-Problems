c2793d542126c44724d024ebf0468723e07b66ee
Mohamed Elfar <melfar391@gmail.com>
2021-07-01 22:55:10 +0200

    permutation algorithm

---
+++ Medium/4- 46. Permutations/46. Permutations.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> perm;
+        do
+        {
+            perm.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+
+        return perm;
+    }
+};
