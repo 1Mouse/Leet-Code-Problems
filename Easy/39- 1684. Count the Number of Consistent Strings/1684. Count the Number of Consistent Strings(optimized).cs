f04570c5a8c0f9e1d73f26aea088febe6fe833c3
Mohamed Elfar <melfar391@gmail.com>
2021-07-31 09:22:40 +0200

    naive solution

---
+++ Easy/39- 1684. Count the Number of Consistent Strings/1684. Count the Number of Consistent Strings(optimized).cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CountConsistentStrings(string allowed, string[] words) {
+        
+        int count = words.Length; // assuming all words are valid
+        var hashSet = new HashSet<char>(allowed);
+        
+        for(int i = 0; i < words.Length; i++) {
+            for(int j = 0; j < words[i].Length; j++) {
+                if(!hashSet.Contains(words[i][j])) {
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
