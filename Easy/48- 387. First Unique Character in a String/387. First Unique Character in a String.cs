2cbd4fd9eecd2aca168b7f3b492ff53411b733e7
Mohamed Elfar <melfar391@gmail.com>
2022-02-13 10:38:57 +0200

    Dictionary, ternary operator

--- Easy/48- 387. First Unique Character in a String/387. First Unique Character in a String.cs
+++ Easy/48- 387. First Unique Character in a String/387. First Unique Character in a String.cs
@@ -1,24 +1,24 @@
 // time: O(n) n=string length
 // space: O(26)~O(1) the alphabet
 public class Solution {
-        public int FirstUniqChar(string s)
-        {
-            // a frequency map <char,{index of char in the string, freq}>
-            var map = new Dictionary<char, List<int>>();
+    public int FirstUniqChar(string s)
+    {
+        // a frequency map <char,{index of char in the string, freq}>
+        var map = new Dictionary<char, List<int>>();
 
-            for (int i = 0; i < s.Length; i++)
-                if (!map.ContainsKey(s[i]))
-                    map.Add(s[i], new List<int>() { i, 1 });
-                else
-                    map[s[i]][1]++;
+        for (int i = 0; i < s.Length; i++)
+            if (!map.ContainsKey(s[i]))
+                map.Add(s[i], new List<int>() { i, 1 });
+            else
+                map[s[i]][1]++;
 
 
-            //to find the first unique char
-            var result = int.MaxValue;
+        //to find the first unique char
+        var result = int.MaxValue;
 
-            for (int i = 0; i < map.Count; i++)
-                result = (map[s[i]][1] == 1 && map[s[i]][0] < result) ? map[s[i]][0] : result;
+        foreach (var item in map)
+            result = (item.Value[1] == 1 && item.Value[0] < result) ? item.Value[0] : result;
 
-            return (result == int.MaxValue) ? -1 : result;
-        }
+        return (result == int.MaxValue) ? -1 : result;
+    }
 }
