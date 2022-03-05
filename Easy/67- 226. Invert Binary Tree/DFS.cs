8867ed1373ba259e0799713ef2014ad388b4306a
Mohamed Elfar <melfar391@gmail.com>
2022-03-05 22:57:41 +0200

    Dfs may overflow

---
+++ Easy/67- 226. Invert Binary Tree/DFS.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+            return null;
+        TreeNode Templeft = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(Templeft);
+        return root;
+    }
+}
