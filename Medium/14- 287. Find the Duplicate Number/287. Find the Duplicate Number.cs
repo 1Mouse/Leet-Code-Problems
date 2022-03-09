f20af43fc00c63bd1f0f45ca856ed9188c5c6817
Mohamed Elfar <melfar391@gmail.com>
2022-03-10 01:10:06 +0200

    sum of nums from 1 to n (Law) (arithmatic progression)

---
+++ Medium/14- 287. Find the Duplicate Number/287. Find the Duplicate Number.cs
@@ -0,0 +1,14 @@
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        int sum = (nums.Length * (nums.Length - 1)) / 2;
+
+        foreach (int num in nums)
+        {
+            sum -= num;
+        }
+
+        return -sum;
+    }
+}
