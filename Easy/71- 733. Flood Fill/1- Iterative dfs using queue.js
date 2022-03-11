ffd2f390365da1386816a46b5dc4ca621405de9b
Mohamed Elfar <melfar391@gmail.com>
2022-03-12 01:17:52 +0200

    not the cleanest but I tried #needs improvment

---
+++ Easy/71- 733. Flood Fill/1- Iterative dfs using queue.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function (image, sr, sc, newColor) {
+    if (image[sr][sc] === newColor) return image;
+    let rows = image.length;
+    let columns = image[0].length;
+    let startingColor = image[sr][sc];
+    let visited = new Set();
+    return dfs(image, sr, sc, rows, columns, startingColor, newColor, visited);
+};
+
+let dfs = function (
+    image,
+    i,
+    j,
+    rows,
+    columns,
+    startingColor,
+    newColor,
+    visited
+) {
+    visited.add([i, j]);
+    let q = [];
+    q.push([i, j]);
+    image[i][j] = newColor;
+
+    while (q.length !== 0) {
+        let current = q.pop();
+
+        for (let direction of directions) {
+            let r = current[0] + direction[0],
+                c = current[1] + direction[1];
+            if (
+                r < rows &&
+                r >= 0 &&
+                c < columns &&
+                c >= 0 &&
+                image[r][c] === startingColor &&
+                !visited.has([r, c])
+            ) {
+                q.push([r, c]);
+                visited.add([r, c]);
+                image[r][c] = newColor;
+            }
+        }
+    }
+    return image;
+};
+var directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
