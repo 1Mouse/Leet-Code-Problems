4d33e8300ed8cfdf3d31a06f0d07472d0bc06772
Mohamed Elfar <melfar391@gmail.com>
2022-07-04 20:31:49 +0200

    prefix sum

--- Easy/82- 724. Find Pivot Index/724. Find Pivot Index.js
+++ Easy/82- 724. Find Pivot Index/724. Find Pivot Index.js
@@ -7,7 +7,7 @@ var pivotIndex = function (nums) {
     array:       1, 7,  3,  6,  5,  6
     prefix sum:  1, 8, 11, 17, 22, 28
     The pivot index is the index where leftSum = rightSum
-    rightSum= totalSum - leftSum- currentValue
+    rightSum = totalSum - leftSum- currentValue
     */
     let sum = 0, leftSum = 0, rightSum = 0;
     nums.forEach(element => {
