07a7fb34b945ce297f58654956039416629216ff
Mohamed Elfar <melfar391@gmail.com>
2022-10-23 06:26:23 +0200

    hashmap, sliding window, great problem

---
+++ Hard/1- 76. Minimum Window Substring/76. Minimum Window Substring.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function (s, t) {
+    let mapT = new Map();
+    for (let i = 0; i < t.length; i++) {
+        mapT.set(t[i], (mapT.has(t[i]) ? 1 + mapT.get(t[i]) : 1));
+    }
+
+    let matched = 0, start = 0, minLength = s.length + 1, subStr = 0;
+    for (let end = 0; end < s.length; end++) {
+        if (mapT.has(s[end])) {
+            mapT.set(s[end], mapT.get(s[end]) - 1);
+            matched = (mapT.get(s[end]) === 0) ? ++matched : matched;
+        }
+
+        while (matched === mapT.size) {
+            let currentLengthOfSubstring = end - start + 1;
+            if (currentLengthOfSubstring < minLength) {
+                minLength = currentLengthOfSubstring;
+                subStr = start;
+            }
+
+            let deleted = s[start++];
+            if (mapT.has(deleted)) {
+                matched = (mapT.get(deleted) === 0) ? --matched : matched;
+                mapT.set(deleted, mapT.get(deleted) + 1);
+            }
+        }
+    }
+    return minLength > s.length ? "" : s.substring(subStr, subStr + minLength);
+};
