7bad38b05a82022e2614bdcdb0fbc5bc308371bb
Mohamed Elfar <melfar391@gmail.com>
2022-10-24 17:25:55 +0200

    smart solution using arry indexing

---
+++ Easy/85- 645. Set Mismatch/645. Set Mismatch.ts
@@ -0,0 +1,9 @@
+function findErrorNums(nums: number[]): number[] {
+    let N = nums.length, ans:number[] = [0,0]
+    for (let i = 0; i < N; i++)
+        nums[(nums[i] - 1) % 10000] += 10000
+    for (let i = 0; i < N; i++)
+        if (nums[i] > 20000) ans[0] = i + 1
+        else if (nums[i] < 10001) ans[1] = i + 1
+    return ans;
+};
